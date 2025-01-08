3a8916229054a95bb372cccabeae1090e989e993
Sahil Raj Verma <sahilrv196@gmail.com>
2025-01-08 23:51:28 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -2,19 +2,26 @@ const sendToken = (user, statusCode, res) => {
   const token = user.getJWTToken();
 
   const options = {
-    expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
+    expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    secure: true, // Only for HTTPS
-  sameSite: "none", // Required for cross-site cookies
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "None",
+    maxAge: 3600000,
   };
 
-  res.status(statusCode).cookie("token", token, options).json({
-    success: true,
-    user,
-    token,
-  });
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({
+      success: true,
+      message: "Authenticated successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    });
 };
 
 module.exports = sendToken;
